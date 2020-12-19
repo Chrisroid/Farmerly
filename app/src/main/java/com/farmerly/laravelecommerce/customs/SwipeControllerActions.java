ff8617a5418067aa025a108d0a7bf43d134e14ac
John Osezele <johnosezele@gmail.com>
2020-12-19 16:20:18 +0100

    1. Committing Farmerly Directory

---
+++ app/src/main/java/com/farmerly/laravelecommerce/customs/SwipeControllerActions.java
@@ -0,0 +1,7 @@
+package com.farmerly.laravelecommerce.customs;
+
+public abstract class SwipeControllerActions {
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+}
